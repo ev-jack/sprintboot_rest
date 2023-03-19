ca14e9e73c6d066f67fd198fe3edcd2b20660654
ev-jack <jolyjeck@gmail.com>
2023-03-19 17:26:10 +0200

    Initial commit

---
+++ src/main/java/com/yevhen/spring/springboot/sprintboot_rest/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.yevhen.spring.springboot.sprintboot_rest.service;
+
+import com.yevhen.spring.springboot.sprintboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
---
+++ src/test/java/com/yevhen/spring/springboot/sprintboot_rest/SprintbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yevhen.spring.springboot.sprintboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
